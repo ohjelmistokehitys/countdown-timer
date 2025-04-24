712ad27a086a01b94e43e8a70df1b8c34bcb1ce6
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2025-04-24 21:39:51 +0300

    Initial commit, static website without countdown

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Timer from "./Timer.tsx";
+
+function App() {
+    return (
+        <main className="container">
+            <article>
+                <h1>Timer</h1>
+                <Timer />
+            </article>
+        </main>
+    );
+}
+
+export default App;
